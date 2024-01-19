3fa05a115ac80b43677852652294306a3c7bbe3a
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-01-19 13:50:39 +0700

    Add segments migration

---
+++ database/migrations/2024_01_19_064559_segments.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $table->id();
+        $table->foreign('irrigation_id')->references('id')->on('irrigations');
+        $table->string('name');
+        $table->string('length');
+        $table->lineString('geom');
+        $table->point('center_point');
+        $table->timestamps();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('segments');
+    }
+};
