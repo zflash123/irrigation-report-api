c35e465ddc4ca42fd4c6e0ba4f9d6bbf87606c27
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-01-19 13:45:14 +0700

    Add irrigations migration

---
+++ database/migrations/2024_01_19_063439_irrigations.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $table->id();
+        $table->foreign('district_id')->references('id')->on('districts');
+        $table->foreign('sub_district_id')->references('id')->on('sub_districts');
+        $table->string('length');
+        $table->string('width');
+        $table->lineString('geom');
+        $table->string('type');
+        $table->string('name');
+        $table->string('condition');
+        $table->string('canal');
+        $table->timestamps();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('irrigations');
+    }
+};
