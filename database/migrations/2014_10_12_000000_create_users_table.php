44fcb5fd022b77018c90da43ad07e34470637383
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-01-19 13:15:11 +0700

    Fix users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreign('urole_id')->references('id')->on('user_roles');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('fullname');
+            $table->string('shortname');
+            $table->string('avatar');
+            $table->string('gender');
+            $table->string('age');
+            $table->string('phone');
+            $table->string('status');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
