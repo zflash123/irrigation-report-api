21a89b0685489e7df296b51e3e3010c08fb90c1f
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-01-22 16:59:49 +0700

    Fix model import and add a function in AppController

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Segment;
 
 class AppController extends Controller
 {
-    //
+    function all_segment() {
+        return Segment::all();
+    }
 }
