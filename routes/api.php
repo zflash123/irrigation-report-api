52c1567cbd291bf0e505291d4b914a8846767f91
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-01-22 14:48:24 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/all-segments', [AppController::class, 'all_segment']);
+
+Route::post('/report', [AppController::class, 'report']);
+
+Route::get('/segments-by-user-id', [AppController::class, 'segments_by_user_id']);
+
+Route::get('/user-data', [AppController::class, 'user_data']);
