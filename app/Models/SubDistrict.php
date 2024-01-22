214ff32d07f56b49db4a617045870863ad6c197f
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-01-22 14:20:06 +0700

    Add SubDistrict model

---
+++ app/Models/SubDistrict.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubDistrict extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'district_id',
+        'name'
+    ];
+}
