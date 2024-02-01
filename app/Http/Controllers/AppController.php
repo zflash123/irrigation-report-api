49d38786803bc5c260816f3be121762e484b47fb
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-02-01 11:34:31 +0700

    Add incomplete API to get one segment from DB

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Segment;
+use Illuminate\Support\Facades\DB;
 
 class AppController extends Controller
 {
     function all_segment() {
-        return Segment::all();
+        $all_segment = DB::select("SELECT *, ST_AsGeoJSON(ST_Transform(geom, 4326)) as geojson FROM master.segments WHERE id='870664d1-213f-4f6b-98b1-bc4528c601c7' LIMIT 30");
+
+        return response()->json($all_segment);
     }
 }
