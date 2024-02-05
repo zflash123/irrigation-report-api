7e9822af206acff9e2f5c6e8afb99475b88c8ea4
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-02-05 16:55:34 +0700

    Fix all_segment API and add report API

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -5,12 +5,32 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Segment;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class AppController extends Controller
 {
     function all_segment() {
-        $all_segment = DB::select("SELECT *, ST_AsGeoJSON(ST_Transform(geom, 4326)) as geojson FROM master.segments WHERE id='870664d1-213f-4f6b-98b1-bc4528c601c7' LIMIT 30");
+        $all_segment = DB::table('master.irrigations_segmen')->where('id', '0000c2b6-7984-4f1f-9385-dcf6e53743a0')->get();
 
         return response()->json($all_segment);
+    }
+    function report(Request $request) {
+        $segment_id = $request->segment_id;
+        $no_ticket = "ticket_".Str::random(100);
+        $note = $request->note;
+        $damage_severity = $request->damage_severity;
+        $photo = "report_".Str::random(100);
+        DB::table('report.report_list')->insert([
+            'segment_id' => $segment_id,
+            'user_id' => 'da9c0989-1f98-4f1c-9f66-571762074fba',
+            'status_id' => '623fb9aa-5d45-4657-b315-2a6c89d725b6',
+            'no_ticket' => $no_ticket,
+            'note' => $note,
+            'maintenance_by' => null,
+            'survey_status' => null,
+            'damage_severity' => $damage_severity,
+            'photo' => $photo,
+        ]);
+        return response()->json($request->all());
     }
 }
