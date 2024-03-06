0e8d931c4b280a2483d6ba6cae75c1ca0ce20bbd
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-03-06 16:02:32 +0700

    Use HasUuids instead of model property for efficiency; change fillable column name from rel_path to file_url

--- app/Models/File/UploadDump.php
+++ app/Models/File/UploadDump.php
@@ -4,20 +4,19 @@ namespace App\Models\File;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class UploadDump extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
     protected $table = 'file.upload_dump';
-    protected $keyType = 'string';
-    public $incrementing = true;
 
     protected $fillable  = [
         'filename',
         'file_type',
         'size',
         'folder',
-        'rel_path',
+        'file_url',
         'uploader_ip',
         'uploader_status',
         'created_at',
