a40f0d4d4dd0be071f6e30a5f9e8ce45cd3d8367
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-03-13 14:07:11 +0700

    Add edit profile API

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -13,4 +13,27 @@ class ProfileController extends Controller
         $user = User::where('id', $user_id)->first();
         return response()->json($user);
     }
+    function edit_profile(Request $request) {
+        try{
+            $user_id = auth()->user()->id;
+            $user = User::where('id', $user_id)->first();
+            $validatedData = $request->validate([
+                'fullname' => 'sometimes|string|max:100',
+                'shortname' => 'sometimes|string|max:100',
+                'phone' => 'sometimes|string|max:13'
+            ]);
+            $update_user = $user->update($validatedData);
+            return response()->json(["user"=>$update_user]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'error' => 'Validation failed',
+                'message' => $e->errors(),
+            ], 422);
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => 'Failed to update your profile',
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
 }
