eba1fce6aefae538863bba00bfd4eb382330d29f
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-06-08 10:15:17 +0700

    Add image upload feature in edit_profile function

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\File;
+use App\FirebaseStorage;
+use App\Models\File\UploadDump;
 
 class ProfileController extends Controller
 {
@@ -18,12 +21,51 @@ class ProfileController extends Controller
             $user_id = auth()->user()->id;
             $user = User::where('id', $user_id)->first();
             $validatedData = $request->validate([
-                'fullname' => 'sometimes|string|max:100',
-                'shortname' => 'sometimes|string|max:100',
-                'phone' => 'sometimes|string|max:13'
+                'fullname' => 'required|string|max:100',
             ]);
-            $update_user = $user->update($validatedData);
-            return response()->json(["user"=>$update_user]);
+            $fullname = $request->fullname;
+            $image = $request->image;
+            $user_ip = $request->ip();
+            function uploadImage($image, $user_ip) {
+                $parts = explode(';', $image);
+                $mimePart = explode(':', $parts[0]);
+                $mime = end($mimePart);
+                $imageExtension = explode('/', $mime)[1];
+                if($imageExtension=="php"){
+                    return null;
+                }
+                $image = preg_replace('#^data:image/\w+;base64,#i', '', $image);
+                $image = str_replace(' ', '+', $image);
+                $imageName = uniqid().'.'.$imageExtension;
+                $imagePath = public_path('images'). '/' . $imageName;
+                $image = base64_decode($image);
+                File::put($imagePath, $image);
+                $storage = FirebaseStorage::initialize();
+                $bucket = $storage->bucket('irrigation-upload-dump.appspot.com');
+        
+                $bucket->upload(fopen($imagePath, 'r'), [
+                    'name' => 'image/' . $imageName,
+                    'predefinedAcl' => 'publicRead'
+                ]);
+                $file_url = 'https://storage.googleapis.com/irrigation-upload-dump.appspot.com/image/' . $imageName;
+                $uploadDump = UploadDump::create([
+                    'filename' => $imageName,
+                    'file_type' => File::extension($imagePath),
+                    'size' => File::size($imagePath),
+                    'folder' => 'image',
+                    'file_url' => $file_url,
+                    'uploader_ip' => $user_ip,
+                    'uploader_status' => true,
+                ]);
+                File::delete($imagePath);
+                return $file_url;
+            }
+            $user->fullname = $fullname;
+            if($image!=null){
+                $user->avatar = uploadImage($image, $user_ip);
+            }
+            $user->save();
+            return response()->json(["message"=>"The user data has been updated"]);
         } catch (ValidationException $e) {
             return response()->json([
                 'error' => 'Validation failed',
